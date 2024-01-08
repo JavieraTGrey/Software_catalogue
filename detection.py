4c5a400022bd81b90639a9869f93657837e634f6
JavieraTGrey <javieratgrey@gmail.com>
2024-01-08 16:10:50 -0300

    arreglo de thresh y kernel

--- detection.py
+++ detection.py
@@ -1,49 +1,53 @@
 from astropy.io import fits
-from astropy.convolution import Gaussian2DKernel, Tophat2DKernel
-import numpy as np
-import sep
-from scipy.ndimage import gaussian_filter
+from astropy.convolution import Gaussian2DKernel
 from matplotlib.patches import Ellipse
 import matplotlib.pyplot as plt
-
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.stats import norm
+import sep
 
 weighted = fits.getdata('weighted.fits')
 w = fits.getdata('w.fits')
 
-# Hago una detección de las fuentes
+# Reviso el thresh en data
+data, bins = np.histogram(weighted.flatten(),
+                          bins=np.linspace(-0.1, 0.005, 300))
 
-# Creación de kernel gaussiano
-fwhm = 3.5  # 3.5 pixeles
-sigma = fwhm / (2 * np.sqrt(2 * np.log(2)))
-gaussian_kernel = gaussian_filter(np.ones((10, 10)), sigma)
 
-# Normalizo kernel
-gaussian_kernel /= np.sum(gaussian_kernel)
+def gaussian(x, amplitude, mean, std_dev):
+    return amplitude * np.exp(-(x - mean)**2 / (2 * std_dev**2))
+
+
+params, conv = curve_fit(gaussian, bins[:-1], data)
+amplitude, mean, std = params
+thresh = 2 * np.sqrt(2 * np.log(2)) * std
+
+kernel = np.array(Gaussian2DKernel(3.5/2.35))
 
 # detección
 weighted = weighted.byteswap().newbyteorder()
-sep.set_extract_pixstack(weighted.size)
+sep.set_extract_pixstack(10000000)
 
-# thresh debe ser del orden del data!
-
-objects = sep.extract(weighted, thresh=1e-9, minarea=3,
-                      deblend_cont=0.0001, clean='N',
-                      filter_kernel=gaussian_kernel,
+objects = sep.extract(weighted, thresh=thresh, minarea=3,
+                      deblend_cont=0.0001, clean=False,
+                      clean_param=1.0,
+                      err=std,
+                      filter_kernel=kernel,
                       filter_type='matched')
 
 
-# Visualización de fuentes detectadas
-fig, ax = plt.subplots()
-m, s = np.mean(weighted), np.std(weighted)
-im = ax.imshow(weighted, interpolation='nearest', cmap='gray',
-               vmin=-1e-09, vmax=4e-8, origin='lower')
-
-# plot an ellipse for each object
-for i in range(len(objects)):
-    e = Ellipse(xy=(objects['x'][i], objects['y'][i]),
-                width=6*objects['a'][i],
-                height=6*objects['b'][i],
-                angle=objects['theta'][i] * 180. / np.pi)
-    e.set_facecolor('none')
-    e.set_edgecolor('red')
-    ax.add_artist(e)
+# Visualización de las detecciones
+# fig, ax = plt.subplots()
+# m, s = np.mean(weighted), np.std(weighted)
+# im = ax.imshow(weighted, interpolation='nearest', cmap='gray',
+#                vmin=m-s, vmax=m+s, origin='lower')
+# # plot an ellipse for each object
+# for i in range(len(objects)):
+#     e = Ellipse(xy=(objects['x'][i], objects['y'][i]),
+#                 width=6*objects['a'][i],
+#                 height=6*objects['b'][i],
+#                 angle=objects['theta'][i] * 180. / np.pi)
+#     e.set_facecolor('none')
+#     e.set_edgecolor('red')
+#     ax.add_artist(e)
