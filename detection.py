e36848b187f583af6c72acd9de54f2e59617f445
JavieraTGrey <javieratgrey@gmail.com>
2024-01-09 12:27:40 -0300

    busca catalogo

--- detection.py
+++ detection.py
@@ -1,10 +1,11 @@
 from astropy.io import fits
 from astropy.convolution import Gaussian2DKernel
+from astropy.coordinates.matching import match_coordinates_sky
+from astropy.wcs import WCS
 from matplotlib.patches import Ellipse
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-from scipy.stats import norm
 import sep
 
 weighted = fits.getdata('weighted.fits')
@@ -20,19 +21,18 @@ def gaussian(x, amplitude, mean, std_dev):
 
 
 params, conv = curve_fit(gaussian, bins[:-1], data)
-amplitude, mean, std = params
-thresh = 2 * np.sqrt(2 * np.log(2)) * std
+amplitude, mean, std_dv = params
+
 
 kernel = np.array(Gaussian2DKernel(3.5/2.35))
 
-# detección
+# Detección
 weighted = weighted.byteswap().newbyteorder()
 sep.set_extract_pixstack(10000000)
 
-objects = sep.extract(weighted, thresh=thresh, minarea=3,
+objects = sep.extract(weighted, thresh=1.2*std_dv, minarea=3,
                       deblend_cont=0.0001, clean=False,
                       clean_param=1.0,
-                      err=std,
                       filter_kernel=kernel,
                       filter_type='matched')
 
@@ -51,3 +51,39 @@ objects = sep.extract(weighted, thresh=thresh, minarea=3,
 #     e.set_facecolor('none')
 #     e.set_edgecolor('red')
 #     ax.add_artist(e)
+
+
+# catalog reading
+cat = fits.open('UNCOVER_DR2_LW_SUPER_catalog.fits')
+data = cat[1].data
+
+column = np.array([4303, 10045])
+line = np.array([5280, 9455])
+
+# Coordenadas de las fuentes del catalogo en pixeles de la imagen original
+x, y = data['x'], data['y']
+
+# Buscar el cuadrado
+select = (x < 10045) & (x > 4303) & (y > 5280) & (y < 9455)
+
+line_real = x[select]
+column_real = y[select]
+
+
+# Agrupo en columna y linea
+real = (column_real, line_real)
+
+# Correccion por el pixel inicial del cuadrado
+detec = (objects['y'] + column[0], objects['x'] + line[0])
+
+# Cambio a coordenadas WCS a partir de un filtro 
+header = fits.getheader('/Images/A2744_F356W.fits', 0)
+wcs_world = WCS(header)
+detcoords = wcs_world.pixel_to_world(detec[0], detec[1])
+realcoords = wcs_world.pixel_to_world(real[0], real[1])
+
+
+# Uso match_coordinates_sky para comparar el catalogo
+index, sep2d, dist3d = match_coordinates_sky(detcoords, realcoords)
+# Busco Matches
+catalog_matches = realcoords[index]
