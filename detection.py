aa32f73bbe050925c73819f5e87ac01f987f8233
JavieraTGrey <javieratgrey@gmail.com>
2024-01-15 17:23:39 -0300

    minor changes

--- detection.py
+++ detection.py
@@ -6,64 +6,91 @@ import sep
 
 
 def gaussian(x, amplitude, mean, std_dev):
-    ''' 1-D Gaussian function
-    INPUT:  x: data
-            amplitude: Function amplitude
-            mean: Expected mean value
-            std_dev: Value for standard deviation
+    ''' 1-D Gaussian function.
+
+    INPUT:
+    - x (array): Data.
+    - amplitude (float): Function amplitude.
+    - mean (float): Expected mean value.
+    - std_dev (float): Standard deviation.
+
     OUTPUT:
-            A*e^(-(x-mean)**2/(2*std_dev**2))
+    - array: A * exp(-(x - mean)**2 / (2 * std_dev**2))
     '''
     return amplitude * np.exp(-(x - mean)**2 / (2 * std_dev**2))
 
 
-def bkg_stimate(data, bkg_func, stop):
-    """ Background estimation for pixel error using a provided function
-    INPUT: data: Data array
-           bkg_func: Function to estimate in the background.
-                     Default is gaussian.
-           stop: Data stop on the histogram to fit
+def bkg_estimate(data, bkg_func=None, stop=0.005):
+    """Estimate background using a provided function.
 
-    OUTPUT: params : fitted parameters of the bkg_func
+    INPUT:
+    - data (array): Data array.
+    - bkg_func (function, optional): Background estimation function.
+                                     Default is Gaussian.
+    - stop (float, optional): Data stop on the histogram to fit.
+                              Default is 0.005.
+
+    OUTPUT:
+    - params (array): Fitted parameters of the background function.
     """
+    if bkg_func is None:
+        bkg_func = gaussian  # Default background function
 
-    dato, bins = np.histogram(data.flatten(),
-                              bins=np.linspace(-0.1, stop, 300))
+    # Calculate histogram
+    hist_data, bins = np.histogram(data.flatten(),
+                                   bins=np.linspace(-0.1, stop, 300))
 
-    params, conv = curve_fit(bkg_func, bins[:-1], dato)
+    # Fit background function to the histogram
+    params, _ = curve_fit(bkg_func, bins[:-1], hist_data)
     return params
 
 
-def detection(path, stop, fwhm, thresh, bkg_func=gaussian, err=None,
+def detection(path, stop, fwhm, thresh,
+              bkg_func=gaussian, err=None,
               kernel_func=Gaussian2DKernel, mask=None,
               minarea=3, deblend_count=0.0001, deblend_nthresh=32,
               clean=False, clean_param=1.0, filter_type='matched',
               segmentation_map=False):
 
-    """ Detect sources on an fits image using sep.
-    INPUT: path: path to the fits file
-           stop: float to stop the data histogram to get the error on the
-                 bkg estimation
-           fwhm: FWHM of the kernel to use
-           bkg_func(optional): Function to use for the bkg estimation.
-                               Default is gaussian
-           kernel_func(optional): Function to use in kernel.
-                                Default is astropy.convolution.Gaussian2DKernel
-           sep_params(optional): Paremeters for sep.detection
+    """Detect sources on a fits image using sep.
+
+    INPUT:
+        - path (str): Path to the fits file.
+        - stop (float): Float to stop the data histogram
+                        for estimating the background.
+        - fwhm (float): FWHM of the kernel to use.
+        - bkg_func (function, optional): Function for background estimation.
+                                         Default is gaussian.
+        - kernel_func (function, optional): Function for the kernel.
+                    Default is astropy.convolution.Gaussian2DKernel.
+        - err (array-like, optional): Error array. Default is None.
+        - mask (array-like, optional): Mask array. Default is None.
+        - minarea (int, optional): Minimum area for detected objects.
+                                   Default is 3.
+        - deblend_count (float, optional): Deblend constant. Default is 0.0001.
+        - deblend_nthresh (int, optional): Deblend threshold. Default is 32.
+        - clean (bool, optional): Perform cleaning. Default is False.
+        - clean_param (float, optional): Cleaning parameter. Default is 1.0.
+        - filter_type (str, optional): Filter type. Default is 'matched'.
+        - segmentation_map (bool, optional): Return segmentation map.
+                                             Default is False.
+
     OUTPUT:
-            Objects: Extracted object parameters (structured array).
-            seg(optional): Array of integers with same shape as data.
-                           Only if segmentation_map is True
-            std_dv: STD fitted for the bkg_func
+        - objects (structured array): Extracted object parameters.
+        - seg (array): Segmentation map (if segmentation_map is True).
+        - std_dv (float): STD fitted for the bkg_func.
     """
-    data = fits.getdata(path)
+    try:
+        data = fits.getdata(path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File not found: {path}")
     kernel = np.array(kernel_func(fwhm/2.35))
 
     # BIG IMAGE
     data = data.byteswap().newbyteorder()
     sep.set_extract_pixstack(10000000)
     if err is None:
-        params = bkg_stimate(data, bkg_func, stop)
+        params = bkg_estimate(data, bkg_func, stop)
         amplitude, mean, std_dv = params
         if segmentation_map is False:
             objects = sep.extract(data, thresh=thresh, minarea=minarea,
